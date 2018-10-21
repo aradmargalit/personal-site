3a7df43b787c2243d127cc7f0a900f7ebbd2dea6
Arad <aradmar@gmail.com>
2018-10-21 08:44:50 -0700

    about structure

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope, faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 import About from './components/About/About';
 import MenuBar from './components/MenuBar/MenuBar';
 import Footer from './components/Footer/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faEnvelope,
+  faCodeBranch,
+  faBackspace,
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faEnvelope, faCodeBranch);
+library.add(faEnvelope, faCodeBranch, faBackspace);
 
 class App extends Component {
   render() {
